ba08e01b2975a86ba508017a3d567f874effc3e1
Nick VanMeter <nickjvm@gmail.com>
2025-07-25 14:59:46 -0600

    fix typo in default prod API url

--- src/lib/serverFetch.ts
+++ src/lib/serverFetch.ts
@@ -1,7 +1,7 @@
 const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
   (process.env.NODE_ENV === "production"
-    ? "https://api.willittaco.com/api"
+    ? "https://willittaco.com/api"
     : "http://localhost:3000/api");
 export default async function serverFetch<T>(
   _url: string,
